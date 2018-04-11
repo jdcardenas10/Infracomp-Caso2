acfe6148977d77e52e08926bd3b0ab14d98554af
jdcardenas10 <jd.cardenas10@uniandes.edu.co>
2018-04-11 10:22:23 -0500

    sdfasdfasd

--- src/servidorCliente/Cliente.java
+++ src/servidorCliente/Cliente.java
@@ -1,6 +1,48 @@
 package servidorCliente;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+
 public class Cliente {
 
-	
+	@SuppressWarnings("resource")
+	public static void main(String args[]){
+		
+		String simetrico = args[0];
+		String hmac = args[1];
+		
+		try {
+			Socket socket=new Socket("localhost",19999);
+			PrintWriter printer=new PrintWriter(socket.getOutputStream(),true);
+			BufferedReader reader=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			
+			Protocolo protocolo = new Protocolo();
+			protocolo.setAlgSimetrico(simetrico);
+			protocolo.setAlgHmac(hmac);
+			
+			protocolo.procesarCadena(null);
+			
+			String respuesta;
+			while ((respuesta = reader.readLine()) != null) {
+				System.out.println("Servidor contestó: " + respuesta);
+				String salida = protocolo.procesarCadena(respuesta);
+				if (salida != null) {
+					System.out.println("Cliente enviando: " + salida);
+					printer.println(salida);
+				}
+			}	
+			
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
--- src/servidorCliente/Protocolo.java
+++ src/servidorCliente/Protocolo.java
@@ -1,5 +1,50 @@
 package servidorCliente;
 
-public class Protocolo {
+import javax.xml.ws.ProtocolException;
 
+public class Protocolo {
+	
+	private int estado;
+	
+	private String algSimetrico;
+	private String algAsimetrico;
+	private String algHmac;
+	
+	private static final int INICIAR_SESION = 0;
+	private static final int AUTENTICAR_CLIENTE_INICIO = 1;
+	private static final int AUTENTICAR_CLIENTE_RESPUESTA = 2;
+
+	public void setAlgSimetrico(String simetrico) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setAlgHmac(String hmac) {
+		algHmac=hmac;
+		
+	}
+
+	public String procesarCadena(String respuesta) {
+		switch(estado){
+		case INICIAR_SESION:
+			if (respuesta == null) {
+				return "HOLA";
+			} else if (respuesta.equals("INICIO")) {
+				return "ALGORITMOS:" + algSimetrico + ":" + algAsimetrico + ":" + algHmac;
+			} else if(respuesta.equals("ESTADO:OK")){
+				estado=AUTENTICAR_CLIENTE_INICIO;
+				return "CERTCLNT";
+			} else {
+				throw new ProtocolException("La entrada :" + respuesta + " no es valida en el inicio");
+			}
+		case AUTENTICAR_CLIENTE_INICIO:
+			estado=AUTENTICAR_CLIENTE_RESPUESTA;
+			return null;
+		case AUTENTICAR_CLIENTE_RESPUESTA:
+			if(respuesta.equals("ESTADO:OK")){
+				return null;
+			}
+		}
+		return null;
+	}	
 }
